54913114e9de0fbf0814c6c575185f98a3f014b2
henrikra96 <105289631+henrikra96@users.noreply.github.com>
2022-05-11 09:09:31 +0200

    reihe = reihe + 1/Math.pow(i, 2);

--- Main.java
+++ Main.java
@@ -27,7 +27,7 @@ public  class Main
         double reihe = 0;
         for(int i = 1 ; i <= 10 ; i++)
         {
-            reihe += 1/Math.pow(i, 2);
+            reihe = reihe + 1/Math.pow(i, 2);
         }
         System.out.println(reihe);
     }
